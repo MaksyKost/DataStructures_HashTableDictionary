dc8179b552102b6e50689854f1cc6431e2d637d6
dackey-wav <danilkrasotov65@gmail.com>
2025-05-31 22:38:48 +0200

    adds a hash table implementation with AVL trees

---
+++ dictionary_avl.h
@@ -0,0 +1,27 @@
+#ifndef DICTIONARY_AVL_H
+#define DICTIONARY_AVL_H
+
+#include "dictionary.h"
+#include "avl_tree.h"
+
+struct Bucket {
+    AVLTree tree;
+};
+
+class DictionaryAVL : public Dictionary {
+private:
+    Bucket *table;
+    int size;
+    int hash(int key) const;
+
+public:
+    DictionaryAVL(int size = 11);
+    ~DictionaryAVL();
+
+    void insert(int key, int value) override;
+    void remove(int key) override;
+    bool find(int key, int &value) override;
+    void clear() override;
+};
+
+#endif
