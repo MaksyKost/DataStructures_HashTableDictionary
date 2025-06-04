5cc822a6d7814c2e96ad1c742ae76faae24a6192
dackey-wav <danilkrasotov65@gmail.com>
2025-06-04 11:31:21 +0200

    heap updated

--- main.cpp
+++ main.cpp
@@ -1,36 +1,4 @@
 #include <iostream>
 #include "src/heap.h"
 
-int main() {
-    Heap heap;
-
-    // Вставка элементов
-    heap.insert(5, 100);
-    heap.insert(2, 200);
-    heap.insert(8, 300);
-    heap.insert(1, 400);
-
-    std::cout << "Heap after insertions:\n";
-    for (const auto& node : heap.getData()) {
-        std::cout << "key: " << node.key << ", value: " << node.value << std::endl;
-    }
-
-    // find
-    int value;
-    if (heap.find(2, value))
-        std::cout << "Found key 2, value = " << value << std::endl;
-    else
-        std::cout << "Key 2 not found\n";
-
-    // remove
-    heap.remove(2);
-    std::cout << "Heap after removing key 2:\n";
-    for (const auto& node : heap.getData()) {
-        std::cout << "key: " << node.key << ", value: " << node.value << std::endl;
-    }
-
-    heap.clear();
-    std::cout << "Heap after clear, size = " << heap.getData().size() << std::endl;
-
-    return 0;
-}
+int main() {}
